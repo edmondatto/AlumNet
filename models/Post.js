fc5573746dd70d2b0d2cf3a47fa3b93d7c697e01
Edmond Atto <edmond091@gmail.com>
2020-04-22 20:24:33 -0400

    Switch to UUIDv4 scheme for Post PK, tweak model property names

--- models/Post.js
+++ models/Post.js
@@ -1,25 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('post', {
-    title: {
-      type: DataTypes.STRING,
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      autoIncrement: false,
+      primaryKey: true,
     },
     content: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    published: {
+    isPublished: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
+      field: 'is_published',
     },
-    public: {
+    isPublic: {
       type: DataTypes.BOOLEAN,
       defaultValue: true,
+      field: 'is_public',
     }
   });
 
   Post.associate = (models) => {
     Post.belongsTo(models.User, {
       foreignKey: 'authorId',
+      as: 'author',
     });
 
     Post.belongsTo(models.Class);
