4557d333b4581eb3785b67985192b72acac415a9
Edmond Atto <edmond091@gmail.com>
2020-04-23 00:28:11 -0400

    Add routes for fetchPublicPosts, fetchOne, update and delete methods to postRouter

--- routes/post.js
+++ routes/post.js
@@ -3,5 +3,10 @@ const { postController } = require('../controllers');
 const postRouter = require('express').Router();
 
 postRouter.post('/', postController.create);
+postRouter.get('/', postController.fetchPublicPosts);
+postRouter.get('/:postId', postController.fetchOne);
+postRouter.delete('/:postId', postController.delete);
+postRouter.put('/:postId', postController.update);
+
 
 module.exports = postRouter;
