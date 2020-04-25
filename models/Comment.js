78cd0e962b5f5cf3088034c2682fcab30027e42a
Edmond Atto <edmond091@gmail.com>
2020-04-24 23:25:01 -0400

    Change PK to UUIDV4, add isEdited attribute

--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
   const Comment = sequelize.define('comment', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      autoIncrement: false,
+      primaryKey: true,
+    },
+
     content: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
+
     published: {
       type: DataTypes.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
     },
+
+    isEdited: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    }
+
+    // TODO: Add isDeleted?
   });
 
   Comment.associate = (models) => {
